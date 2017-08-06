d60be6151044065e5cf477d165dfd5391bb8a613
Igor T. Ghisi <igor.ghisi@gmail.com>
2017-08-06 13:01:23 -0300

    Remove a folder that seems unused

--- tests/foo/bar/test_hello_submodule.py
+++
@@ -1,7 +0,0 @@
-
-
-def test_read_hello_from_submodule(datadir):
-    filename = datadir / 'hello.txt'
-    with open(str(filename)) as fp:
-        contents = fp.read()
-    assert contents == 'Hello, world!\n'
