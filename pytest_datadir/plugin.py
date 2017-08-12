9688c34c9c354b767ad47e603ce4de12ff3bc03e
Igor T. Ghisi <igor.ghisi@gmail.com>
2017-08-12 00:46:53 -0300

    Fix Path constructor on linux

--- pytest_datadir/plugin.py
+++ pytest_datadir/plugin.py
@@ -8,7 +8,7 @@ import pytest
 @pytest.fixture
 def shared_datadir(request, tmpdir):
     original_shared_path = os.path.join(request.fspath.dirname, 'data')
-    temp_path = pathlib.Path(tmpdir.join('data'))
+    temp_path = pathlib.Path(str(tmpdir.join('data')))
     shutil.copytree(original_shared_path, str(temp_path))
     return temp_path
 
