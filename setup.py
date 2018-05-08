bb63b8beecced86c08d268fc3e50314dcdeb56e8
Bruno Oliveira <nicoddemus@gmail.com>
2018-05-07 21:04:08 -0300

    Test with Python 3.6 as well

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Quality Assurance',
         'Topic :: Software Development :: Testing',
     ],
