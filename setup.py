e481ba9af9768c5c9595b34a9e491aae824d970d
Bruno Oliveira <nicoddemus@gmail.com>
2022-01-18 13:07:08 -0300

    Remove data_files from setup.py
    This is not necessary because `setuptools-scm` already takes care of including the file in the source dist, plus this is causing the conda package to contain a `LICENSE` file in the root of the conda environment, which is conflicting with other packages.

--- setup.py
+++ setup.py
@@ -18,7 +18,6 @@ setup(
     },
     package_dir={"": "src"},
     install_requires=['pytest>=2.7.0'],
-    data_files = [("", ["LICENSE"])],
     author='Gabriel Reis',
     author_email='gabrielcnr@gmail.com',
     description='pytest plugin for test data directories and files',
