f8aa73711cb8058c35c118fee4314afaded4c4b1
marcelotrevisani <marceloduartetrevisani@gmail.com>
2022-09-16 12:23:09 +0100

    Rerun pre-commit for all files

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import io
 
-from setuptools import find_packages, setup
+from setuptools import find_packages
+from setuptools import setup
 
 with io.open("README.md", encoding="UTF-8") as fp:
     long_description = fp.read()
