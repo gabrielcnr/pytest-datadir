718edf3fea893ddb78cf8f7fb79720a6f963f2e9
Bruno Oliveira <nicoddemus@gmail.com>
2025-06-06 08:07:59 -0300

    Update test_pathlib.py

--- tests/test_pathlib.py
+++ tests/test_pathlib.py
@@ -4,7 +4,7 @@ import pytest
 from pytest_datadir.plugin import _win32_longpath
 
 
-def test_win32_longpath_idempotent(datadir):
+def test_win32_longpath_idempotent(datadir: Path) -> None:
     """Double application should not prepend twice."""
     first = _win32_longpath(str(datadir))
     second = _win32_longpath(first)
@@ -14,7 +14,7 @@ def test_win32_longpath_idempotent(datadir):
 @pytest.mark.skipif(
     not sys.platform.startswith("win"), reason="Only makes sense on Windows"
 )
-def test_win32_longpath_unc(datadir):
+def test_win32_longpath_unc(datadir: Path) -> None:
     unc_path = r"\\ComputerName\SharedFolder\Resource"
     longpath = _win32_longpath(unc_path)
     assert longpath.startswith("\\\\?\\UNC\\")
